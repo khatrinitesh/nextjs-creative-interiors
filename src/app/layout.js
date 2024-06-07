74bd7816442f37f9b3374842af9d57bef6fd1ee9
nitesh <nitesh.khatri88@gmail.com>
2024-06-07 16:55:36 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
-        <link type="image/jpeg" rel="icon" href="/public/logo.jpg"/>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
       </head>
       <body className="overflow-x-hidden">
         <div className="flex flex-col min-h-screen">

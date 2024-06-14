98ad849e5020bbfec41d6402c2a7f1496213378b
nitesh <nitesh.khatri88@gmail.com>
2024-06-14 16:25:01 +0530

    updated google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,39 @@
-"use client"
-import React,{useEffect} from 'react'
+"use client";
+import React, { useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 // COMPONENTS
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import ThemeToggle from '@/components/ThemeToggle';
-import WhatsappIcon from '@/components/WhatsappIcon';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ThemeToggle from "@/components/ThemeToggle";
+import WhatsappIcon from "@/components/WhatsappIcon";
 
 // STYLE
-import '../assets/style/globals.css';
+import "../assets/style/globals.css";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
-  // other pages 
+  // other pages
   const validRoutes = ["/", "/about", "/portfolio", "/contact"];
 
   // only home page
-  const isHomePage = pathname === '/';
+  const isHomePage = pathname === "/";
+  
+
+  
+  
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
         <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+
+        <GoogleAnalytics gaId='G-Z239PYXWWK' />
       </head>
       <body className="overflow-x-hidden">
         <div className="flex flex-col min-h-screen">
@@ -31,19 +41,23 @@ export default function RootLayout({ children }) {
           {validRoutes.includes(pathname) && <Header />}
           {/* E > HEADER */}
 
-            {/* S > MAIN CONTENT */}
-            <div className={`mainContent grow  ${isHomePage ? '' : 'md:py-[50px] mt-[75px] md:mt-[114px] lg:mt-0'}`}>
-              {children}
-            </div>
-            {/* E > MAIN CONTENT */}
-            
+          {/* S > MAIN CONTENT */}
+          <div
+            className={`mainContent grow  ${
+              isHomePage ? "" : "md:py-[50px] mt-[75px] md:mt-[114px] lg:mt-0"
+            }`}
+          >
+            {children}
+          </div>
+          {/* E > MAIN CONTENT */}
+
           {/* S > FOOTER */}
           {validRoutes.includes(pathname) && <Footer />}
           {/* E > FOOTER */}
           <ThemeToggle />
-          <WhatsappIcon/>
+          <WhatsappIcon />
         </div>
-        </body>
+      </body>
     </html>
   );
 }

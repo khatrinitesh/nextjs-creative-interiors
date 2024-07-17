28d45acbeaee67c8578928262f8badf3d1823177
nitesh <nitesh.khatri88@gmail.com>
2024-07-17 12:14:21 +0530

    updated

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  siteUrl: 'https://increative.in',
+  changefreq: 'weekly',
+  priority: 0.5,
+  sitemapSize: 5000,
+  exclude: ['/admin/**'], 
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,30 @@
-import React from "react";
+import Head from 'next/head';
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 // STYLE
 import "../assets/style/globals.css";
-import { Link } from 'next/link';
 
 export const metadata = {
   title: "   Creative Interiors - A Complete Design Solution",
   description: "Get More out of Your Home or Office Space. Work with Our Interior Designer Now.",
+  keywords: "Creative Interiors, Design Solution, Interior Designer",
   viewport:"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
 };
-
-
 
 export default function RootLayout({ children }) {
   
   return (
-    <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+    <html>
+        <meta name="viewport" content={metadata.viewport} />
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
         <link rel="icon" href="./favicon.ico" type="image/x-icon" />
         <link rel="canonical" href="https://www.increative.in/" />
 
         <GoogleAnalytics gaId='G-Z239PYXWWK' />
-      </head>
+      </Head>
       <body className="overflow-x-hidden">
         {children}
       </body>
